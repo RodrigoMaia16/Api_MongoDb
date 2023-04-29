3a52f04cf1ad3c9c6014913922d081a71d3c45ea
RodrigoMaia16 <123781177+RodrigoMaia16@users.noreply.github.com>
2023-04-29 14:08:47 -0300

    Add files via upload

---
+++ APilura/src/main/java/br/com/alura/APilura/APiluraApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.APilura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APiluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APiluraApplication.class, args);
+	}
+
+}
---
+++ APilura/src/test/java/br/com/alura/APilura/APiluraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.APilura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class APiluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
